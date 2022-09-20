ba367729649e47f1bae7479604a81fcedb68d25c
slipmip <slipmip@outlook.dk>
2022-09-20 20:11:20 +0200

    format

--- TaskForg.py
+++ TaskForg.py
@@ -1,12 +1,11 @@
 print('Programs:')
-progs=['Conv','Porto','Lam']
+progs = ['Conv', 'Porto', 'Lam']
 print(progs)
 
 
 while True:
 
-
-    if input()=='Conv':
+    if input() == 'Conv':
         euro = 7.43
         minFee = 0.02
         print()
@@ -18,43 +17,40 @@ while True:
         while True:
             print('input DKK')
             dkk = input()
-            if dkk=='back':
+            if dkk == 'back':
                 break
             else:
                 print()
                 print('That is:')
-                dkk=float(dkk)
-                print(str(dkk/euro),'euro')
+                dkk = float(dkk)
+                print(str(dkk/euro), 'euro')
                 print('Conversion fee:')
-                if dkk/euro*minFee>0.5:
+                if dkk/euro*minFee > 0.5:
                     print(dkk/euro*minFee)
                     print()
                 else:
                     print('0.5')
-                    print()                
-    elif input()=='Porto':
+                    print()
+    elif input() == 'Porto':
         print('Porto program started')
         while True:
             print()
             print('this program will calculate the price it will cost you to send a letter from denmark to another country, also know as "A Prioritare"')
             print()
             print('input weight in grams')
-            grams=input()
+            grams = input()
             print()
-            if grams >=100:
+            if grams >= 100:
                 print('you will pay:')
                 print('29.00')
-            if grams >=250:
+            if grams >= 250:
                 print('you will pay:')
                 print('58.00')
-            if grams >=2000:
+            if grams >= 2000:
                 print('you will pay:')
                 print('87.00')
 
-
-    elif input()=='LAM':
+    elif input() == 'LAM':
         print('"Largest And Smallest" number detector started')
         while True:
             print('poop')
-
-
