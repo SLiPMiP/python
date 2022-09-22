8aacc5beb8675dfc4d9187352f6a3bc71119fedc
slipmip <slipmip@outlook.dk>
2022-09-22 13:15:56 +0200

    minor

--- lop.py
+++ lop.py
@@ -1,52 +1,50 @@
 while True:
-    programs=['Countdown','Additive','Multiplication','Function']
+    programs = ['Countdown', 'Additive', 'Multiplication', 'Function']
     print()
     print('Programs')
-    print(programs,' Exit')
-    
-    keyword=input()
+    print(programs, ' Exit')
 
-    if keyword.lower() in ['countdown','count','1']:
+    keyword = input()
+
+    if keyword.lower() in ['countdown', 'count', '1']:
         print()
         print('Self destruct initiated.')
         print('M E L T D O W N     E M I N E N T')
         print()
         print('How long time would you like to have to escape?')
-        maxTime=float(input())
-        countDown=maxTime
-        i=0
-        while i<maxTime:
+        maxTime = float(input())
+        countDown = maxTime
+
+        for i in range(maxTime):
             print(countDown)
-            countDown=countDown-1
-            i=i+1
+            countDown = countDown-1
         print('KABOOM')
-    
-    elif keyword.lower() in ['additive','add','2']:
+
+    elif keyword.lower() in ['additive', 'add', '2']:
         print()
         print('Addition program intiated')
         print('this program adds 0+1+2+3+4.... and you define how many times it does so.')
         print()
         print('input the amount of times youd like the program to run')
-        times=int(input())
+        times = int(input())
         print()
-        j=1
+        j = 1
         # i=1
         # while i<=n:
         #     j=j+i
         #     print(j)
         #     i=i+1
-        
+
         for i in range(times):
-            j=j+i
+            j = j+i
             print(j)
 
-
-    elif keyword.lower() in ['multiplication','mult','3']:
+    elif keyword.lower() in ['multiplication', 'mult', '3']:
         print()
         print('Multiplication table program started.')
         print('this program generates a multiplication table with a given value as the smallest value.')
         print()
         print('input multiplication root and the size of the multiplication table')
-        n=input()
-        n=n.split()
-        i=1
+        n = input()
+        n = n.split()
+        i = 1
